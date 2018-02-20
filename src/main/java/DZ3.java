4e937423f9887234875ad0a28c0c72d71e08ecb6
ychernikov <ychernikov@playtika.com>
2018-02-20 11:27:48 +0200

    Added second and third tasks

--- src/main/java/DZ3.java
+++ src/main/java/DZ3.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
-
+import java.util.regex.Pattern;
 /**
  * Created by admin on 2/10/2018.
  */
+@SuppressWarnings("ALL")
 public class DZ3 {
     static  final char A='A';
     static  final char B='B';
@@ -13,6 +14,8 @@ public class DZ3 {
 
     public static void main(String[] args) {
         getSrting();
+        getRepitedInStr();
+        getCheckValidation();
        /* Программа запрашивает строку пользователя,которую она должна преобразовать согласно
         меню выбора:
         A. СТРОКА ЗАГЛАВНЫМИ БУКВАМИ
@@ -32,7 +35,7 @@ public class DZ3 {
 */
     }
         public static void getSrting() {
-            System.out.printf("Vvedite lubyu stroky:%n");
+            System.out.printf("Vvedite stroky:%n");
             Scanner console = new Scanner(System.in);
             String str = console.nextLine();
             boolean flag=true;
@@ -94,5 +97,85 @@ public class DZ3 {
 
             }}
             return;
-    }}
+    }
+
+    public static void getRepitedInStr() {
+     /*   Задача 2.
+        Пользователь вводит строку. Подсчитать количество повторений символов, во введенной
+        строке. Вводимая строка должна состоять из символов только латинского алфавита.
+                Вывести на экран(построчно) кол-во повторений каждого символа входной строки.
+                */
+        System.out.printf("Vvedite stroky из символов только латинского алфавита:%n");
+        Scanner console = new Scanner(System.in);
+        String str = console.nextLine();
+        int f=str.length();
+        StringBuilder sb = new StringBuilder(str);
+        int count = 1;
+        while (sb.length()!=0) {
+            for (int i = 1; i < sb.length()+1; i++) {
+
+                if (sb.length() >= 1) {
+                    if (i != 1) {
+                        char current = sb.charAt(0);
+                        char previous = sb.charAt(i - 1);
+                        if (current == previous ) {
+                            count++;
+                            sb.deleteCharAt(i - 1);
+                            i = i - 1;
+                            if (sb.length() == 2) {
+                                count=count+1;
+                                sb.deleteCharAt(0);
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.printf(sb.charAt(0) + " :" + count + " " + "%n");
+            sb.deleteCharAt(0);
+            if(sb.length()!=0) {
+                sb.deleteCharAt(0);
+                count = 1;
+            }
+        }
+    }
+
+    public static void getCheckValidation(){
+    /*Задача 3.
+    Программа запрашивает у пользователя ФИО, мобильный телефон и email, и проверяет
+    данные на валидность с помощью регулярных выражений.
+            ФИО - только буквы английского алфавита, дефис и пробел
+    Мобильный телефон - +380 далее 2 цифры кода (украина) и 7 любых цифр
+    email - любые латинские символы, цифры, дефис и нижний прочерк, обязательный символ @
+            и .
+    Проверить все вводимые данные и вывести неверно введенные.*/
+    System.out.printf("Введите ФИО:%n");
+    Scanner console = new Scanner(System.in);
+    String line = console.nextLine();
+    System.out.printf("%nВведите номер мобильного телефона:%n");
+    String line1 = console.nextLine();
+    System.out.printf("%nВведите Ваш e-mail:%n");
+    String line2 = console.nextLine();
+    Pattern fio = Pattern.compile("^([A-Za-z-]+)\\s([A-Za-z-]+)\\s([A-Za-z-]+)$");
+    Pattern mobile = Pattern.compile("(\\+380(50|67|94))\\d{7}");
+    Pattern email = Pattern.compile("^(\\w|\\p{L1})+@(\\w|\\p{all})+\\.([A-Za-z]{2,7})");
+    System.out.println(fio.matcher(line).matches());
+    boolean flag=false;
+    if (!fio.matcher(line).matches()) {
+        System.out.printf("%nВы неверно ввели ФИО: %s%n Используйте, только буквы английского алфавита, дефис", line);
+        flag=true;
+    }
+    if (!mobile.matcher(line1).matches()) {
+        System.out.printf("%nВы неверно ввели мобильный: %s%n Укажите номер в формате +380 далее 2 цифры кода (украина) и 7 цифр номера", line1);
+        flag=true;
+    }
+    if (!email.matcher(line2).matches()) {
+        System.out.printf("%nВы неверно ввели ФИО: %s%n Введите email в формате <любые символы>@<любые символы>.домен состоящий только из букв от 2 до 7 символов", line2);
+        flag=true;
+    }
+    if (flag=false) {
+        System.out.printf("%nСпасибо. Вы все ввели правильно!!!");
+    }
+    }
+
+}
 
