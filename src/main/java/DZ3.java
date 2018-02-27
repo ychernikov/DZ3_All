c5711b5ec0b9789f34e7cf8583efadff381b24ca
ychernikov <ychernikov@playtika.com>
2018-02-27 11:20:27 +0200

    Changed printf format

--- src/main/java/DZ3.java
+++ src/main/java/DZ3.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
+
 /**
  * Created by admin on 2/10/2018.
  */
 @SuppressWarnings("ALL")
 public class DZ3 {
-    static  final char A='A';
-    static  final char B='B';
-    static  final char C='C';
-    static  final char D='D';
-    static  final char E='E';
-    static  final char F='F';
+    static final char A = 'A';
+    static final char B = 'B';
+    static final char C = 'C';
+    static final char D = 'D';
+    static final char E = 'E';
+    static final char F = 'F';
 
     public static void main(String[] args) {
         getSrting();
@@ -34,35 +35,38 @@ public class DZ3 {
                 Для выбора варианта использовать оператор switch.
 */
     }
-        public static void getSrting() {
-            System.out.printf("Vvedite stroky:%n");
-            Scanner console = new Scanner(System.in);
-            String str = console.nextLine();
-            boolean flag=true;
-            if(flag) {
-                flag=false;
+
+    public static void getSrting() {
+        System.out.printf("Vvedite stroky:%n");
+        Scanner console = new Scanner(System.in);
+        String str = console.nextLine();
+        boolean flag = true;
+        if (flag) {
+            flag = false;
+            System.out.printf("%nA. СТРОКА ЗАГЛАВНЫМИ БУКВАМИ%nB. строка в нижнем регистре%nC. С Заглавной Буквы (Первый Символ Каждого Слова В Строке )%n");
+            System.out.printf("D. пЕРВЫЙ сИМВОЛ в нИЖНЕМ рЕГИСТРЕ%nE. Как в предложении(с заглавной буквы)%nF. выход из программы%n");
+        }
+        String group = console.nextLine().toUpperCase();
+        boolean flag1 = true;
+        while (flag1) {
+
+            if (group.charAt(0) != A && group.charAt(0) != B
+                    && group.charAt(0) != C && group.charAt(0) != D
+                    && group.charAt(0) != E && group.charAt(0) != F) {
                 System.out.printf("%nA. СТРОКА ЗАГЛАВНЫМИ БУКВАМИ%nB. строка в нижнем регистре%nC. С Заглавной Буквы (Первый Символ Каждого Слова В Строке )%n");
                 System.out.printf("D. пЕРВЫЙ сИМВОЛ в нИЖНЕМ рЕГИСТРЕ%nE. Как в предложении(с заглавной буквы)%nF. выход из программы%n");
+                group = console.nextLine().toUpperCase();
             }
-            String group = console.nextLine().toUpperCase();
-            boolean flag1=true;
-            while (flag1){
-
-                if (group.charAt(0)!=A && group.charAt(0)!=B && group.charAt(0)!=C && group.charAt(0)!=D && group.charAt(0)!=E && group.charAt(0)!=F) {
-                    System.out.printf("%nA. СТРОКА ЗАГЛАВНЫМИ БУКВАМИ%nB. строка в нижнем регистре%nC. С Заглавной Буквы (Первый Символ Каждого Слова В Строке )%n");
-                    System.out.printf("D. пЕРВЫЙ сИМВОЛ в нИЖНЕМ рЕГИСТРЕ%nE. Как в предложении(с заглавной буквы)%nF. выход из программы%n");
-                    group = console.nextLine().toUpperCase();
-                    }
 
             char choice = group.charAt(0);
-            switch(choice) {
+            switch (choice) {
                 case A:
-                    System.out.printf("%nVash vubor СТРОКА ЗАГЛАВНЫМИ БУКВАМИ:%n" + str.toUpperCase());
-                    flag1=false;
+                    System.out.printf("%nVash vubor СТРОКА ЗАГЛАВНЫМИ БУКВАМИ:%n%s", str.toUpperCase());
+                    flag1 = false;
                     break;
                 case B:
-                    System.out.printf("%nVash vubor строка в нижнем регистре:%n" + str.toLowerCase());
-                    flag1=false;
+                    System.out.printf("%nVash vubor строка в нижнем регистре:%n%s",str.toLowerCase());
+                    flag1 = false;
                     break;
                 case C:
 
@@ -73,7 +77,7 @@ public class DZ3 {
                         sb.setCharAt(0, splittedStr[i].toUpperCase().charAt(0));
                         String.format("%s ", "%nVash vubor С Заглавной Буквы (Первый Символ Каждого Слова В Строке ):%n" + System.out.printf(splittedStr[i].valueOf(sb) + " "));
                     }
-                    flag1=false;
+                    flag1 = false;
                     break;
                 case D:
                     String[] splittedStr1 = str.toUpperCase().split(" ");
@@ -83,20 +87,21 @@ public class DZ3 {
                         sb.setCharAt(0, splittedStr1[i].toLowerCase().charAt(0));
                         String.format("%s ", "%nVash vubor пЕРВЫЙ сИМВОЛ в нИЖНЕМ рЕГИСТРЕ:%n" + System.out.printf(splittedStr1[i].valueOf(sb) + " "));
                     }
-                    flag1=false;
+                    flag1 = false;
                     break;
                 case E:
                     StringBuilder sb = new StringBuilder(str);
                     sb.setCharAt(0, str.toUpperCase().charAt(0));
-                    System.out.printf("%nVash vubor Как в предложении(с заглавной буквы):%n" + sb + " ");
-                    flag1=false;
+                    System.out.printf("%nVash vubor Как в предложении(с заглавной буквы):%n%s", sb);
+                    flag1 = false;
                     break;
                 case F:
-                    flag1=false;
+                    flag1 = false;
                     break;
 
-            }}
-            return;
+            }
+        }
+        return;
     }
 
     public static void getRepitedInStr() {
@@ -108,22 +113,22 @@ public class DZ3 {
         System.out.printf("Vvedite stroky из символов только латинского алфавита:%n");
         Scanner console = new Scanner(System.in);
         String str = console.nextLine();
-        int f=str.length();
+        int f = str.length();
         StringBuilder sb = new StringBuilder(str);
         int count = 1;
-        while (sb.length()!=0) {
-            for (int i = 1; i < sb.length()+1; i++) {
+        while (sb.length() != 0) {
+            for (int i = 1; i < sb.length() + 1; i++) {
 
                 if (sb.length() >= 1) {
                     if (i != 1) {
                         char current = sb.charAt(0);
                         char previous = sb.charAt(i - 1);
-                        if (current == previous ) {
+                        if (current == previous) {
                             count++;
                             sb.deleteCharAt(i - 1);
                             i = i - 1;
                             if (sb.length() == 2) {
-                                count=count+1;
+                                count = count + 1;
                                 sb.deleteCharAt(0);
                             }
                         }
@@ -132,14 +137,14 @@ public class DZ3 {
             }
             System.out.printf(sb.charAt(0) + " :" + count + " " + "%n");
             sb.deleteCharAt(0);
-            if(sb.length()!=0) {
+            if (sb.length() != 0) {
                 sb.deleteCharAt(0);
                 count = 1;
             }
         }
     }
 
-    public static void getCheckValidation(){
+    public static void getCheckValidation() {
     /*Задача 3.
     Программа запрашивает у пользователя ФИО, мобильный телефон и email, и проверяет
     данные на валидность с помощью регулярных выражений.
@@ -148,33 +153,33 @@ public class DZ3 {
     email - любые латинские символы, цифры, дефис и нижний прочерк, обязательный символ @
             и .
     Проверить все вводимые данные и вывести неверно введенные.*/
-    System.out.printf("Введите ФИО:%n");
-    Scanner console = new Scanner(System.in);
-    String line = console.nextLine();
-    System.out.printf("%nВведите номер мобильного телефона:%n");
-    String line1 = console.nextLine();
-    System.out.printf("%nВведите Ваш e-mail:%n");
-    String line2 = console.nextLine();
-    Pattern fio = Pattern.compile("^([A-Za-z-]+)\\s([A-Za-z-]+)\\s([A-Za-z-]+)$");
-    Pattern mobile = Pattern.compile("(\\+380(50|67|94))\\d{7}");
-    Pattern email = Pattern.compile("^(\\w|\\p{L1})+@(\\w|\\p{all})+\\.([A-Za-z]{2,7})");
-    System.out.println(fio.matcher(line).matches());
-    boolean flag=false;
-    if (!fio.matcher(line).matches()) {
-        System.out.printf("%nВы неверно ввели ФИО: %s%n Используйте, только буквы английского алфавита, дефис", line);
-        flag=true;
-    }
-    if (!mobile.matcher(line1).matches()) {
-        System.out.printf("%nВы неверно ввели мобильный: %s%n Укажите номер в формате +380 далее 2 цифры кода (украина) и 7 цифр номера", line1);
-        flag=true;
-    }
-    if (!email.matcher(line2).matches()) {
-        System.out.printf("%nВы неверно ввели ФИО: %s%n Введите email в формате <любые символы>@<любые символы>.домен состоящий только из букв от 2 до 7 символов", line2);
-        flag=true;
-    }
-    if (flag=false) {
-        System.out.printf("%nСпасибо. Вы все ввели правильно!!!");
-    }
+        System.out.printf("Введите ФИО:%n");
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+        System.out.printf("%nВведите номер мобильного телефона:%n");
+        String line1 = console.nextLine();
+        System.out.printf("%nВведите Ваш e-mail:%n");
+        String line2 = console.nextLine();
+        Pattern fio = Pattern.compile("^([A-Za-z-]+)\\s([A-Za-z-]+)\\s([A-Za-z-]+)$");
+        Pattern mobile = Pattern.compile("(\\+380(50|67|94))\\d{7}");
+        Pattern email = Pattern.compile("^(\\w|\\p{L1})+@(\\w|\\p{all})+\\.([A-Za-z]{2,7})");
+        System.out.println(fio.matcher(line).matches());
+        boolean flag = false;
+        if (!fio.matcher(line).matches()) {
+            System.out.printf("%nВы неверно ввели ФИО: %s%n Используйте, только буквы английского алфавита, дефис", line);
+            flag = true;
+        }
+        if (!mobile.matcher(line1).matches()) {
+            System.out.printf("%nВы неверно ввели мобильный: %s%n Укажите номер в формате +380 далее 2 цифры кода (украина) и 7 цифр номера", line1);
+            flag = true;
+        }
+        if (!email.matcher(line2).matches()) {
+            System.out.printf("%nВы неверно ввели ФИО: %s%n Введите email в формате <любые символы>@<любые символы>.домен состоящий только из букв от 2 до 7 символов", line2);
+            flag = true;
+        }
+        if (flag = false) {
+            System.out.printf("%nСпасибо. Вы все ввели правильно!!!");
+        }
     }
 
 }
